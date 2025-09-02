1672f589911f0dbff3d5ed1e3d241f23eb32ef85
DeanHawley <retrogamer202202@gmail.com>
2025-09-02 17:55:02 -0400

    Cookie Clicker Steam.js

--- Cookie Clicker Steam.js
+++ Cookie Clicker Steam.js
@@ -1,8 +1,8 @@
 if (ccsm === undefined) var ccsm = {};
 Game.registerMod("ccsm", {
   info: {
-    Name: "CCSM",
-    Description: "Meant to replicate cookie clicker for steam"
+    Name: "Cookie Clicker Steam",
+    Description: "Meant to replicate Cookie Clicker for Steam"
   },
   init: function () {
     ccsm.version = "1.01";
@@ -20,10 +20,7 @@ Game.registerMod("ccsm", {
       if (e.length > 1) e[1].remove();
 
       let f = l("buffs");
-      if (f) {
-        f.style.marginTop = "20px";
-        f.style.top = "16px";
-      }
+      if (f) f.style.top = "16px";
 
       let g = document.querySelector("#store").previousElementSibling;
       if (g) g.remove();
@@ -97,47 +94,128 @@ Game.registerMod("ccsm", {
     Steam.allowSteamAchievs = true;
     Steam.mods = {};
     Steam.modList = [];
-})();
 
-(function(){
-if(typeof Game==='undefined')return;
+    Steam.writeCloudUI = () => {
+        return '<div class="listing" style="margin-bottom:-8px;">'
+            + Game.WritePrefButton('cloudSave','cloudSaveButton',"Cloud saving ON","Cloud saving OFF",'')
+            + '<label>(allow use of Steam Cloud for save backups)</label></div>'
+            + '<div id="cloudIsOn" class="listing" style="display:none;">'
+            + '<a class="option" style="font-size:11px;margin-left:12px;">Purge Cloud</a>'
+            + '<label>Current Cloud use: <b>?</b></label></div>'
+            + '<div id="cloudIsOff" class="listing" style="display:inline-block;font-size:11px;margin-left:12px;color:#c00;">No Cloud access at the moment.</div>'
+            + '<div class="listing">'
+            + Game.WritePrefButton(
+                'discordPresence',
+                'discordPresenceButton',
+                "Discord status ON",
+                "Discord status OFF",
+                'Steam.toggleRichPresence(Game.prefs.discordPresence);'
+            )
+            + '<label>(if Discord is on, show your game info as activity status)</label></div>';
+    };
 
-var locSafe = function(id,params){
-    if(typeof loc==='function')return loc(id,params);
-    if(typeof params!=='undefined'){
-        if(Array.isArray(params)){
-            var out=id;
-            for(var i=0;i<params.length;i++) out=out.replace('%'+(i+1),params[i]);
-            return out;
+    Steam.writeModUI = () => {
+        return `<div style="text-align:center;">
+            <a style="text-align:center;margin:4px;" class="option smallFancyButton" onclick="Steam.modsPopup();PlaySound('snd/tick.mp3');">${loc("Manage mods")}</a>
+            <a style="text-align:center;margin:4px;" class="option smallFancyButton" onclick="Game.CheckModData();PlaySound('snd/tick.mp3');">${loc("Check mod data")}</a>
+            <a style="text-align:center;margin:4px;" class="option smallFancyButton" onclick="Steam.workshopPopup();PlaySound('snd/tick.mp3');">${loc("Publish mods")}</a>
+        </div>`;
+    };
+
+    Steam.modsPopup = function()
+    {
+        let mods = [];
+        for (let id in Game.mods)
+        {
+            let mod = Game.mods[id];
+            mods.push({
+                name: mod.info?.Name || id,
+                id: id,
+                desc: mod.info?.Description || "",
+                dependencies: mod.dependencies || [],
+                disabled: mod.disabled || false,
+                loc: true,
+                workshop: true
+            });
         }
-        return id.replace('%1',params);
-    }
-    return id;
-};
 
-var log = Game.updateLog || '';
+        function renderModList(selectedIndex = -1)
+        {
+            let listEl = l("modList");
+            if (!mods.length)
+            {
+                listEl.innerHTML = `<div style="font-size:11px;opacity:0.5;margin:8px;">(${loc("No mods loaded")})</div>`;
+                return;
+            }
+            let html = "";
+            mods.forEach((mod, i) => {
+                html += `<div class="zebra mouseOver${i === selectedIndex ? ' selected pucker' : ''}" style="padding:4px;${mod.disabled ? 'opacity:0.5;background:rgba(255,0,0,0.2);' : ''}" id="mod-${i}"><b>${mod.name}</b></div>`;
+            });
+            listEl.innerHTML = html;
+            mods.forEach((mod, i) => {
+                AddEvent(l(`mod-${i}`), "click", () => {
+                    renderModOptions(mod, i);
+                });
+            });
+        }
 
-function insertAfterTitle(title,html){
-    var needle = '<div class="title">' + title + '</div>';
-    var i = log.indexOf(needle);
-    if(i === -1) return false;
-    var pos = i + needle.length;
-    if(log.indexOf(html, pos) !== -1) return false;
-    log = log.slice(0, pos) + html + log.slice(pos);
-    return true;
-}
+        function renderModOptions(mod, index)
+        {
+            let el = l("modOptions");
+            el.innerHTML = `
+                <div class="name">${mod.name}</div>
+                <div class="tag">ID: ${mod.id}</div>
+                <div class="line"></div>
+                <a class="option" id="modDisable">${mod.disabled ? loc("Enable") : loc("Disable")}</a>
+                <a class="halfLeft option off">${loc("Priority up")}</a>
+                <a class="halfRight option off">${loc("Priority down")}</a>
+                ${mod.loc ? `<a class="option" id="modOpenFolder">${loc("Open folder")}</a>` : ""}
+                ${mod.desc ? '<textarea readonly style="margin:4px;padding:4px 8px;width:80%;box-sizing:border-box;height:80px;font-size:11px;">' + mod.desc + '</textarea>' : ""}
+                <div class="line" style="clear:both;"></div>
+                ${mod.workshop ? `
+                    <a class="option" id="modOpenWorkshop">${loc("Open Workshop page")}</a>
+                    <a class="option" id="modUnsubscribeWorkshop">${loc("Unsubscribe")}</a>
+                ` : `<div style="opacity:0.5;margin:4px;">${loc("Local mod")}</div>`}
+            `;
+        }
 
-var aboutInsert = '<div class="listing" style="font-weight:bold;font-style:italic;opacity:0.5;">' + locSafe("Note: links will open in your web browser.") + '</div>' + '<div class="listing">' + locSafe("Music by %1.",'<a href="https://twitter.com/C418" target="_blank">C418</a>') + '</div>';
-if(log.indexOf('Music by') === -1) insertAfterTitle(locSafe("About"), aboutInsert);
+        Game.Prompt(`
+            <id ManageMods>
+            <h3>${loc("Manage mods")}</h3>
+            <div class="line"></div>
+            <div style="overflow:hidden;clear:both;">
+                <a class="option" style="float:left;">${loc("Open /mods folder")}</a>
+                <a class="option" style="float:right;">${loc("Open Workshop")}</a>
+            </div>
+            <div style="font-size:11px;opacity:0.7;">${loc("Mods are loaded from top to bottom.")}</div>
+            <div class="line"></div>
+            <div style="height:300px;width:100%;position:relative;margin:12px 0px;">
+                <div class="inner" style="font-size:11px;height:100%;width:50%;overflow-y:scroll;position:absolute;left:0px;" id="modList"></div>
+                <div class="tight" style="font-size:11px;height:100%;width:50%;overflow-y:auto;position:absolute;right:0px;padding-left:10px;" id="modOptions">
+                    ${loc("Select a mod.")}
+                </div>
+            </div>
+        `, [[loc("Restart with new changes"), 0, 'display:none;'], loc("Back")], 0, 'widePrompt');
+        renderModList();
+    };
 
-insertAfterTitle('07/05/2023 - often imitated, never duplicated','<div class="listing">&bull; removed Discord rich presence support (plugin currently broken)</div>');
-insertAfterTitle('31/05/2022 - a mind of its own','<div class="listing">&bull; new option to disable your game activity showing up in Discord</div><div class="listing">&bull; launch errors now provide the option to restart without mods</div>');
-
-if(log.indexOf('18/12/2021 - work it') === -1){
-    log += ('</div><div class="subsection update small"><div class="title">18/12/2021 - work it</div><div class="listing">&bull; added Steam Workshop support (lets you install mods and upload your own)</div><div class="listing">&bull; added Korean language support</div><div class="listing">&bull; added a few more features for the Steam build</div>');
-}
-
-log = log.replace(/<div class="listing block"[^>]*id="supportSection"[^>]*>[\s\S]*?<\/div>/, '');
-
-Game.updateLog = log;
+    Steam.workshopPopup = function()
+    {
+        Game.Prompt(`
+            <id PublishMods>
+            <h3>${loc("Publish mods")}</h3>
+            <div class="line"></div>
+            <div class="block" id="modDisplay">
+                <div style="font-size:11px;">
+                    ${tinyIcon([16,5])}<div></div>
+                    ${loc("This tool allows you to upload new mods to the Steam Workshop.<br>You need to select a mod folder containing a properly-formatted %1 file.<br>See the included sample mods for examples.",'<b>info.txt</b>')}
+                </div>
+                <div class="line"></div>
+                <div style="overflow:hidden;clear:both;">
+                    <a class="option" style="display:block;font-weight:bold;">${loc("New mod")} - ${loc("Select folder")}</a>
+                    <a class="option" style="display:block;font-weight:bold;">${loc("Update published mods")}...</a>
+                </div>
+            </div>
+        `, [[loc("Back"), 'Steam.workshopPopup();', 'display:none;'], loc("Cancel")], 0, 'widePrompt');
+    };
 })();
